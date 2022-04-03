bc6f6d7354fbfc8441ecdfb23e4e22e67dd0e771
Fran Terrón <fpterronm01@educarex.es>
2022-04-03 18:46:07 +0200

    Day 1 - List Launches

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import { Heading, Image } from "@chakra-ui/react";
+import { LaunchItem } from "./components/LaunchItem"
+import * as API from "./services/launches";
+import logo from "./assets/logo-spacex.svg";
+
+export function App() {
+  const [launches, setLaunches] = useState([]);
+
+  useEffect(() => {
+    API.getAllLaunches().then(setLaunches);
+  }, []);
+
+  return (
+    <>
+      <Image src={logo} width={500} m={4} />
+      <Heading align="center" as="h1" size="lg" m={4}>SpaceX Launches</Heading>
+      <section>
+          {launches.map(launch => (
+            <LaunchItem key={launch.flight_number} {...launch}/>
+          ))}
+      </section>
+    </>
+  );
+}
+
